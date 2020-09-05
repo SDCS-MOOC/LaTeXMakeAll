27181c624e0b584f3f3ef548a005d15e4a3ed37d
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-09-06 00:09:31 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import argparse
+import pathlib
+import os
+
+def build_parser() -> argparse.ArgumentParser:
+    parse = argparse.ArgumentParser()
+    parse.add_argument("--root", default="", type=str)
+    parse.add_argument("--glob", default="**/*.tex", type=str)
+    return parse
+
+if __name__ == "__main__":
+    arg = build_parser().parse_args()
+    cwd = os.getcwd()
+    cmd = "latexmk -xelatex -file-line-error -halt-on-error -interaction=nonstopmode -shell-escape -jobname={0} -usepretex=\'{1}\' {2}"
+    rootpath = pathlib.Path(arg.root)
+    for f in rootpath.glob(arg.glob):
+        basename = os.path.dirname(f)
+        sourcename = f.name
+
+        os.chdir(f.parent)
+        os.system(cmd.format(basename, "", sourcename))
+        os.system(cmd.format(basename+"-handout", "\\PassOptionsToClass\{handout\}\{beamer\}", sourcename))
+
+        os.chdir(cwd)
