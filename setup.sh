94d39554edd4e944606d67c885ad422389890e8b
MilkFather <31627231+MilkFather@users.noreply.github.com>
2020-09-06 02:20:10 +0800

    use /usr/share/fonts

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ set -ex
 
 wget -nv "https://github.com/alif-type/xits/releases/download/v1.302/XITS-1.302.zip"
 unzip "XITS-1.302.zip"
-find "XITS-1.302" -name "*.otf" -exec mv '{}' /usr/local/share/fonts \;
+find "XITS-1.302" -name "*.otf" -exec mv '{}' /usr/share/fonts \;
 fc-cache -fv
